76d274817e1078eb98c4a352a8e400a7e561a7cb
Ralph Hogenbirk <info@ralphdesign.nl>
2014-01-16 12:00:01 +0100

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialsearch::Application.config.secret_key_base = 'ebf1e4cdbaf004e29fb43b607a629bcc869ac34ced1107641ba2d1809445a4ae219c95260ab58ae970a823101feaeeb4f8cf034a404d4b051607cd5d77ece6cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialsearch::Application.config.session_store :cookie_store, key: '_socialsearch_session'
