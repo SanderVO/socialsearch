660a7004743424e3682f728cf40ffb49c26f43bb
Ralph Hogenbirk <info@ralphdesign.nl>
2014-01-17 13:44:54 +0100

    twitter shit

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,5 @@
+client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = "jrQQLPvLRzJ9lLf6pd8r3Q"
+		  config.consumer_secret     = "s5ylJSbIyX8t51bZIZY14hTwVFoG9k3SIUPbe6cNJo"
+		  config.bearer_token        = "AAAAAAAAAAAAAAAAAAAAAIAETwAAAAAAgWaQPsghbCxRF5NAl%2FdfiagCVaE%3DjQ4CHqMZP6LwfWFmeARDgL0uTVD5x184l2UivAVGR5I2LkumrU"
+		end
